3a60d1488917b6759ce5a532e10db8bf0d299e05
k1jeong <kjeong522@gmail.com>
2022-02-24 23:21:45 +0900

    Completion block / 딜레이 처리 / awesome-swift 애니메이션

--- CompletionBlock-Tutorial/ViewController.swift
+++ CompletionBlock-Tutorial/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var mainTitle: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		print("ViewController - viewDidLoad() called")
+		
+		KRProgressHUD.show()
+		
+		sayHi(completion: { result in
+			print("컴플레션 블럭으로 넘겨 받았음: \(result)")
+			KRProgressHUD.showSuccess()
+			
+			self.mainTitle.text = result
+		})
 	}
 
-
+	
+	fileprivate func sayHi(completion: @escaping(String) -> ()){
+		
+		print("ViewController - sayHi() called")
+		
+		DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+			// Code you want to be delayed
+			//print("Hi!")
+			completion("Hi")
+		}
+	}
+	
 }
 
